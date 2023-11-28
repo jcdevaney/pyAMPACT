477704160fed25c561aad577a0e6258267e1ffe7
Alexander Morgan <alexanderpmorgan@gmail.com>
2023-11-28 11:30:39 -0500

    Add .xmlIDs()
    Works for simple pieces, but needs to accommodate chord objects too.

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ import requests
 import os
 import tempfile
 import re
+import xml.etree.ElementTree as ET
 m21.environment.set('autoDownload', 'allow')
 
 function_pattern = re.compile('[^TtPpDd]')
@@ -316,6 +317,41 @@ class Score:
       self._analyses['harmKeys'] = pd.Series()
     if 'cdata' not in self._analyses:
       self._analyses['cdata'] = pd.DataFrame()
+
+  def xmlIDs(self):
+    '''\tReturn xml ids per part in a pandas.DataFrame time-aligned with the
+    objects offset.'''
+    # TODO: make this work with chords and divisi too.
+    if self.fileExtension not in ('xml', 'mei'):
+      return pd.DataFrame(index=self._parts().index, columns=self._parts().columns)
+    tree = ET.parse(self.path)
+    root = tree.getroot()
+    namespace = {'ns': root.tag.split('}')[0].strip('{')}
+    idString = [key for key in root.attrib.keys() if key.endswith('}id')][0]
+    data = {}
+    dotCoefficients = {None: 1, '1': 1.5, '2': 1.75, '3': 1.875, '4': 1.9375}
+    for staff in root.findall('.//ns:staff', namespace):
+        for layer in staff.findall('ns:layer', namespace):
+            column_name = f"Staff{staff.get('n')}_Layer{layer.get('n')}"
+            if column_name not in data:
+                data[column_name] = []
+            for nrb in layer:
+              if nrb.tag.endswith('note') or nrb.tag.endswith('rest') or nrb.tag.endswith('mRest'):
+                xml_id = nrb.get(idString)
+                data[column_name].append(xml_id)
+              elif nrb.tag.endswith('beam'):
+                for nr in nrb:
+                  xml_id = nr.get(idString)
+                  data[column_name].append(xml_id)
+    ids = pd.DataFrame.from_dict(data, orient='index').T
+    cols = []
+    parts = self._parts()
+    for i in range(len(parts.columns)):
+      part = parts.iloc[:, i].dropna()
+      idCol = ids.iloc[:, i].dropna()
+      idCol.index = part.index
+      cols.append(idCol)
+    return pd.concat(cols, axis=1)
 
   def lyrics(self):
     if 'lyrics' not in self._analyses:
